8d0e5853b47fdb155b51015ffbbb50d3c83af8e2
Rita0156 <ritachaudhari006@gmail.com>
2024-02-16 08:36:39 +0530

    table

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import TopBarMenu from './AllBarFolder/Topbar';
 import AdminTablebar from './AllBarFolder/adminTablebar';
+import Responsivebar from './AllBarFolder/Responsivebar';
 
 function App() {
   return (
     <div className="App">
         {/* <TopBarMenu/> */}
-          <AdminTablebar/>
+         <Responsivebar/>
     </div>
   );
 }
