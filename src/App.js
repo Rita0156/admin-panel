7c2c5c49750c631a07036911e48b3755b9339e42
Rita0156 <ritachaudhari006@gmail.com>
2024-02-17 08:30:54 +0530

    mn

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import TopBarMenu from './AllBarFolder/Topbar';
 import AdminTablebar from './AllBarFolder/adminTablebar';
 import Responsivebar from './AllBarFolder/Responsivebar';
+import DummyData from './dummytable';
+import BasicExample from './dummy2';
 
 function App() {
   return (
     <div className="App">
         {/* <TopBarMenu/> */}
-         <Responsivebar/>
+         {/* <Responsivebar/> */}
+         <BasicExample/>
     </div>
   );
 }
